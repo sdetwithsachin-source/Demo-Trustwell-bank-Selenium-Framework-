81d46550a88e72b438676861abb0278c097c628c
sdetwithsachin-source <sdetwithsachin@gmail.com>
2026-06-13 15:57:24 +0530

    First commit

---
+++ src/test/java/pageObjects/DashboardPage.java
@@ -0,0 +1,27 @@
+package pageObjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DashboardPage extends BasePage {
+
+    public DashboardPage(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "transferBtn")
+    WebElement btnTransferMoney;
+
+    // Alternative locator
+    // @FindBy(xpath = "//button[@data-testid='transfer-btn']")
+    // WebElement btnTransferMoney;
+
+    public void clickTransferMoney() {
+        btnTransferMoney.click();
+    }
+
+    public boolean isTransferMoneyButtonDisplayed() {
+        return btnTransferMoney.isDisplayed();
+    }
+}
